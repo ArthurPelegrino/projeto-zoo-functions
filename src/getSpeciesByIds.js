5753aa589bba6b9f6bda486bd4ac74fa9e7beedb
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2022-08-01 22:21:27 -0300

    Requisito 3 let's gooo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,6 @@ const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => data.species.filter((especie) => ids
   .find((myIds) => myIds === especie.id));
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
