3daebaeb8b4459dc972fd748d307ff2652af4a20
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2022-08-01 21:54:02 -0300

    Correção erros de lint requisito 1 e requisito 2 pronto!

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animaSpecies = species.find((especie) => especie.name === animal);
+  return animaSpecies.residents.every((idade) => idade.age >= age);
 }
 
+console.log(getAnimalsOlderThan('lions', 11));
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,7 @@
-
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => {
-  return data.species.filter((especie) => ids.find((idQueEuTomandando) => idQueEuTomandando  === especie.id));
-}
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
+const getSpeciesByIds = (...ids) => data.species.filter((especie) => ids
+  .find((myIds) => myIds === especie.id));
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
