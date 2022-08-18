34ae20892c1abc00192b81c9d5e9fc9636c8df0a
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2022-08-17 23:55:27 -0300

    tetando de novo, agora vai

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -26,4 +26,15 @@ describe('Testes da função HandlerElephants', () => {
   it('para o argumento diferente de string deve retornar "Parâmetro inválido, é necessário uma string"', () => {
     expect(handlerElephants(!String)).toMatch('Parâmetro inválido, é necessário uma string');
   });
+  it('teste', () => {
+    expect(handlerElephants()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
 });
